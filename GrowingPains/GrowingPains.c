7ba1153466c9f4bce164a6db940d6f891ab67556
Jed Powell <jedojedi@hotmail.com>
2025-02-22 16:11:30 +0000

    Add project files.

---
+++ GrowingPains/GrowingPains.c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	// Get values into array
+	int nums[5], numsLength = sizeof(nums) / 4;
+
+	for (int x = 0; x < numsLength; x++) {
+		printf("Insert a number: ");
+		scanf("%d", &nums[x]);
+	}
+
+	// Combine values
+	int combinedNums = 0, position = 10000; // position in tens, hundreds, etc
+
+	for (int x = 0; x < numsLength; x++) {
+		combinedNums += nums[x] * position;
+		position = position / 10;
+	}
+
+	printf("Combined number: %d\n", combinedNums);
+	
+	// Separate values into variables
+	int num1 = combinedNums / 10000;
+	int num2 = combinedNums / 1000 % 10;
+	int num3 = combinedNums / 100 % 10;
+	int num4 = combinedNums / 10 % 10;
+	int num5 = combinedNums / 1 % 10;
+
+	printf("%d => %d,%d,%d,%d,%d\n", combinedNums, num1, num2, num3, num4, num5);
+
+	return 0;
+}
---
+++ GrowingPains/README.md
@@ -0,0 +1,7 @@
+#Growing Pains
+
+Write a program that reads 5 int values (must be between 0-9) from the user and then
+combines them into a single 5 digit int value, e.g. 1,2,3,4,5 => 12345. Output this value
+to the screen and then reverse the process by turning the same 5 digit int value back
+into 5 separate int values which should be output to the screen in the following format:
+12345 => 1,2,3,4,5
